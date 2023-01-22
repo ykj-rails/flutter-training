bb6bc022f06c451292d14aa6aa6fb256e0196d4d
ykj-rails <yukiji.0307@gmail.com>
2023-01-22 10:46:07 +0900

    MyHomePageリファクタリング

--- lib/pages/MyHomePage/index.dart
+++ lib/pages/MyHomePage/index.dart
@@ -1,26 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:yumemi_weather/yumemi_weather.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
+import 'package:yumemi_weather/yumemi_weather.dart';
 
 // MyHomePageのState
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
