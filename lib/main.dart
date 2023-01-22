ee2c8451a4ff23b733616a1431a8ed33be47b1da
ykj-rails <yukiji.0307@gmail.com>
2023-01-22 10:45:51 +0900

    Myappリファクタリング

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,23 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 // Project imports:
 import 'package:flutter_training/pages/MyHomePage/index.dart';
+import 'package:flutter_training/pages/SplashPage/index.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const SplashPage(),
+    );
+  }
 }
