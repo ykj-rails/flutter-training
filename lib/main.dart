6c33c7f98e236120aeaaf292eb3877cfef7decea
ykj-rails <yukiji.0307@gmail.com>
2023-01-26 09:31:25 +0900

    routes追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:yumemi_weather/yumemi_weather.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 // Project imports:
-import 'package:flutter_training/pages/MyHomePage/index.dart';
 import 'package:flutter_training/pages/SplashPage/index.dart';
+import 'package:flutter_training/pages/WeatherPage/index.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: const SplashPage(),
-    );
-  }
+      routes: <String, WidgetBuilder>{
+        '/weather': (context) => const WeatherPage()
+      },
+    ),
+  );
 }
